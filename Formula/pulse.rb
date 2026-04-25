e7b7484402a4e331eaf2f8267a4d7fd7b8d95705
kin0kaze <k1n0.k423@gmail.com>
2026-04-25 18:20:42 +0900

    pulse 0.3.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/pulse.rb
+++ Formula/pulse.rb
@@ -1,10 +1,10 @@
 class Pulse < Formula
   desc "Safe cleanup and machine audit for macOS developers"
   homepage "https://github.com/kin0kaze23/pulse"
-  url "https://github.com/kin0kaze23/pulse/releases/download/v0.2.9/pulse-universal.zip"
-  sha256 "eb6c025fc19501aeac99ab6f27a09e8a98339c28d6e3d2a593884f2ce9537aa5"
+  url "https://github.com/kin0kaze23/pulse/releases/download/v0.3.0/pulse-universal.zip"
+  sha256 "75f70f15a95af5243e8124fc5f1bd85ba48e802b1db0f87d86db5f1d0c3cf439"
   license "MIT"
-  version "0.2.9"
+  version "0.3.0"
 
   depends_on macos: :sonoma
 
